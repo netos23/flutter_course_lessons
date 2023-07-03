8c34322eb9068aecfe696500225335afb92057b2
netos23 <nikmoroz2002@outlook.com>
2023-07-03 23:54:09 +0300

    feature(dart) add dart syntax root

---
+++ dart_syntax/bin/dart_syntax.dart
@@ -0,0 +1,5 @@
+import 'package:dart_syntax/dart_syntax.dart' as dart_syntax;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_syntax.calculate()}!');
+}
---
+++ dart_syntax/test/dart_syntax_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_syntax/dart_syntax.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
